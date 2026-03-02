7a3a8a7d5669a9311d8fc14b0eabb07e6e733ed9
Eljin <eljinaguilar98@gmail.com>
2026-03-03 00:41:08 +0800

    Set-up testing

--- src/app.js
+++ src/app.js
@@ -36,5 +36,9 @@ app.get('/api', (req, res) => {
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', usersRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route Not Found' });
+});
 export default app;
  
